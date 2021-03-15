a8402f7e371c89cf3df509e0a28f82d0143a1b6f
Stealthbroken <77743603+Stealthbroken@users.noreply.github.com>
2021-03-14 20:55:46 -0400

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'reactionrole',
     description: "Sets up a reaction role message!",
-    async execute(message, args, Discord, client) {
+    async execute(client, message, args, Discord) {
         const channel = '820804774190514176';
         const NorthAmerican = message.guild.roles.cache.find(role => role.name === "North_American");
         const SouthAmerican = message.guild.roles.cache.find(role => role.name === "South_American");
