1338f8ee2b9bcefde28b22f098d97ac9f4287736
Stealthbroken <77743603+Stealthbroken@users.noreply.github.com>
2021-03-17 20:21:16 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,14 +2,16 @@ module.exports = {
     name: 'ban',
     description: "This will ban a player",
     execute(messages, args){
-        const member = messages.mentions.users.first();
-        if (messages.member.hasPermission("KICK_MEMBERS")){
-        if(member){
-            const memberTarget = messages.guild.members.cache.get(member.id)
-            memberTarget.ban();
-            messages.channel.send("User was banned!");
-        }}else{
-            messages.channel.send('You need to mention someone!');
-        }
+messages.channel.send("This Command is a work in progress! Please try again later!");
+
+        //        const member = messages.mentions.users.first();
+//        if (messages.member.hasPermission("BAN_MEMBERS")){
+//        if(member){
+//            const memberTarget = messages.guild.members.cache.get(member.id)
+//            memberTarget.ban();
+//            messages.channel.send("User was banned!");
+//        }}else{
+//            messages.channel.send('You need to mention someone!');
+//        }
     }
 }
