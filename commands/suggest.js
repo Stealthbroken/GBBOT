7ddc1d8b1205e7cb15c65372f5534f5b86a4625f
Stealthbroken <77743603+Stealthbroken@users.noreply.github.com>
2021-03-17 21:32:22 +0000

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,12 +1,10 @@
-const { execute } = require("./genderreactionrole");
-
 module.exports = {
     name: 'suggest',
     aliases: ['suggestions', 'suggestions'],
     permissions: [],
     description: 'Creates a Suggestion',
     execute(message, args, cmd, client, Discord){
-        const channel = message.guild.channels.cache.find(c => c.name === 'suggestion');
+        const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
         if(!channel) return message.channel.send('There is no channel called Suggestions! Please ask the admin or Dev!');
 
     }
