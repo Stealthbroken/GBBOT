a1a1d86e028eefa7f6d54fb659e83c7d569b6682
Stealthbroken <77743603+Stealthbroken@users.noreply.github.com>
2021-03-17 21:47:57 +0000

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -8,7 +8,7 @@ module.exports = {
         if(!channel) return message.channel.send('suggestions channel does not exist!');
 
         let messageArgs = args.join(' ');
-        const embed = new discord.MessageEmbed()
+        const embed = new Discord.MessageEmbed()
         .setColor('FADF2E')
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
         .setDescription(messageArgs);
