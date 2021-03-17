11ee93f566722d8c14c39a3e1b1fe405415c0702
Stealthbroken <77743603+Stealthbroken@users.noreply.github.com>
2021-03-17 21:40:05 +0000

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,11 +1,24 @@
 module.exports = {
     name: 'suggest',
-    aliases: ['suggestions', 'suggestions'],
+    aliases: ['suggestion', 'suggestion'],
     permissions: [],
-    description: 'Creates a Suggestion',
-    execute(message, args, cmd, client, Discord){
+    description: 'creates a suggestion!',
+    execute(message, args, cmd, client, discord){
         const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
-        if(!channel) return message.channel.send('There is no channel called Suggestions! Please ask the admin or Dev!');
+        if(!channel) return message.channel.send('suggestions channel does not exist!');
 
+        let messageArgs = args.join(' ');
+        const embed = new discord.MessageEmbed()
+        .setColor('FADF2E')
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        .setDescription(messageArgs);
+
+        channel.send(embed).then((msg) =>{
+            msg.react('👍');
+            msg.react('👎');
+            message.delete();
+        }).catch((err)=>{
+            throw err;
+        });
     }
 }
