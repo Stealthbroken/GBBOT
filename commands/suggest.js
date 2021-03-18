9287e1754dc93ad4d5aeaad34613010b45757b5d
Stealthbroken <77743603+Stealthbroken@users.noreply.github.com>
2021-03-18 01:48:49 +0000

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,7 +3,7 @@ module.exports = {
     aliases: ['suggestion', 'suggestion'],
     permissions: [],
     description: 'creates a suggestion!',
-    execute(message, args, cmd, client, Discord){
+    execute(message, args, client, Discord){
         const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
         if(!channel) return message.channel.send('suggestions channel does not exist!');
 
